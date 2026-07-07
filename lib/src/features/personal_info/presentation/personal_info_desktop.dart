cb1b4cdee1dd480d04811f21880023ca3664f01d
Mohammed <a.a.mxhamed@gmail.com>
2026-07-08 00:57:50 +0100

    edited template data

--- lib/src/features/personal_info/presentation/personal_info_desktop.dart
+++ lib/src/features/personal_info/presentation/personal_info_desktop.dart
@@ -3,9 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:portfolio/src/constants/sizes.dart';
 import 'package:portfolio/src/features/personal_info/data/personal_info_repository.dart';
-import 'package:portfolio/src/features/personal_info/domain/resume.dart';
 import 'package:portfolio/src/features/personal_info/presentation/widgets/contact_bar.dart';
-import 'package:portfolio/src/features/personal_info/presentation/widgets/resume_button.dart';
 import 'package:portfolio/src/localization/generated/locale_keys.g.dart';
 
 class PersonalInfoDesktop extends ConsumerWidget {
@@ -13,7 +11,6 @@ class PersonalInfoDesktop extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final resumes = ref.watch(personalInfoRepositoryProvider).getResumes();
     final contacts = ref.watch(personalInfoRepositoryProvider).getContacts();
 
     return Column(
@@ -33,23 +30,9 @@ class PersonalInfoDesktop extends ConsumerWidget {
           tr(LocaleKeys.subDescription),
           style: Theme.of(context).textTheme.bodyLarge,
         ),
-        _buildResumeButton(ref, resumes: resumes.toList()),
         const Spacer(),
         gapH8,
         ContactBar(contacts: contacts.toList()),
-      ],
-    );
-  }
-
-  Widget _buildResumeButton(WidgetRef ref, {required List<Resume> resumes}) {
-    if (resumes.isEmpty) return const SizedBox.shrink();
-    return Column(
-      children: [
-        gapH40,
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 24),
-          child: ResumeButton(resumes: resumes),
-        ),
       ],
     );
   }
--- lib/src/features/personal_info/presentation/personal_info_mobile.dart
+++ lib/src/features/personal_info/presentation/personal_info_mobile.dart
@@ -3,9 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:portfolio/src/constants/sizes.dart';
 import 'package:portfolio/src/features/personal_info/data/personal_info_repository.dart';
-import 'package:portfolio/src/features/personal_info/domain/resume.dart';
 import 'package:portfolio/src/features/personal_info/presentation/widgets/contact_bar.dart';
-import 'package:portfolio/src/features/personal_info/presentation/widgets/resume_button.dart';
 import 'package:portfolio/src/localization/generated/locale_keys.g.dart';
 
 class PersonalInfoMobile extends ConsumerWidget {
@@ -13,7 +11,6 @@ class PersonalInfoMobile extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final resumes = ref.watch(personalInfoRepositoryProvider).getResumes();
     final contacts = ref.watch(personalInfoRepositoryProvider).getContacts();
 
     return Column(
@@ -33,18 +30,10 @@ class PersonalInfoMobile extends ConsumerWidget {
           tr(LocaleKeys.subDescription),
           style: Theme.of(context).textTheme.bodyLarge,
         ),
-        _buildResumeButton(ref, resumes: resumes.toList()),
+        gapH64,
         gapH8,
         ContactBar(contacts: contacts.toList()),
       ],
-    );
-  }
-
-  Widget _buildResumeButton(WidgetRef ref, {required List<Resume> resumes}) {
-    if (resumes.isEmpty) return const SizedBox.shrink();
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 28),
-      child: ResumeButton(resumes: resumes),
     );
   }
 }
